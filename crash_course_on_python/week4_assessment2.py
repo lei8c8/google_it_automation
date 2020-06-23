4acd45d73ac9e356272686a12f0a26f4e0b63e6b
Lei Chen <lei8c8@gmail.com>
2020-06-22 20:30:27 -0400

    finished learning week5

--- crash_course_on_python/week4_assessment2.py
+++ crash_course_on_python/week4_assessment2.py
@@ -3,4 +3,4 @@ def highlight_word(sentence, word):
 
 print(highlight_word("Have a nice day", "nice"))
 print(highlight_word("Shhh, don't be so loud!", "loud"))
-print(highlight_word("Automating with Python is fun", "fun"))
+print(highlight_word("Automating with Python is fun", "fun"))[]
